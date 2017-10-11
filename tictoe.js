2d182c15a8ca8ce2880c7032914becb87870c16a
tushar bansal <tuskibansal@gmail.com>
2017-10-11 17:51:50 +0530

    functionality

--- tictoe.js
+++ tictoe.js
@@ -1,6 +1,22 @@
-
-
+var value1 = "X";
+var x ="";
 function myfun(id){
-	console.log(id);
+	
+	
+	x = document.getElementById(id).textContent;
+	if(!x){
+		if(value1 === "X"){
+			document.getElementById(id).innerHTML = value1;
+			value1 = "O";
+		}
+		else{
+			document.getElementById(id).innerHTML = value1;
+			value1 = "X";
+		}
+	}
+	
+
+
+	
 
 }
